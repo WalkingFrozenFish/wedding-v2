11b6142c616d2041c277afcad407108be85c249c
Oleg Chachin <chachin.ol@yandex.kz>
2024-07-05 20:27:13 +0500

    Delete comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Hero from "./components/Hero/Hero.tsx";
 import styles from "./App.module.css"
 import Text from "./components/Text/Text.tsx";
 import Shedule from "./components/Shedule/Shedule.tsx";
-import Flower from "./components/Flower/Flower.tsx";
 import Address from "./components/Address/Address.tsx";
 import Timer from "./components/Timer/Timer.tsx";
 import Form from "./components/Form/Form.tsx";
@@ -16,39 +15,25 @@ function App() {
                 <Hero/>
             </Wrapper>
 
-            
-
-            {/* <Flower> */}
-                
-            {/* </Flower> */}
-
             <Wrapper>
                 <Text />
             </Wrapper>
 
-            {/* <Flower> */}
             <Wrapper>
                 <Shedule />
             </Wrapper>
-            {/* </Flower> */}
 
-            {/* <Flower> */}
             <Wrapper>
                 <Timer />
             </Wrapper>
-            {/* </Flower> */}
 
-            {/* <Flower> */}
             <Wrapper>
                 <Address />
             </Wrapper>
-            {/* </Flower> */}
 
-            {/* <Flower> */}
             <Wrapper>
                 <Form />
             </Wrapper>
-            {/* </Flower> */}
         </div>
     );
 }
